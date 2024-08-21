e1f0af60c3e4a9ccacc47f375618a16ec3a98961
Brady Fomegne <brady.fomegne@outlook.com>
2024-08-21 16:24:01 +0100

    chore: config webpack for dev & prod environment (#33)

--- packages/afrim-input/src/bootstrap.js
+++ packages/afrim-input/src/bootstrap.js
@@ -3,6 +3,9 @@
 // that no one else needs to worry about it again.
 import("./index.js")
   .then((o) => {
+    if (process.env.NODE_ENV !== "production") {
+      console.log("[afrim-input] Looks like we are in development mode!");
+    }
     window.Afrim = o.default;
   })
-  .catch((e) => console.error("Error importing `index.js`:", e));
+  .catch((e) => console.error("Error importing `index.ts`:", e));
--- packages/afrim-playground/src/bootstrap.js
+++ packages/afrim-playground/src/bootstrap.js
@@ -3,6 +3,9 @@
 // that no one else needs to worry about it again.
 import("./index.js")
   .then((o) => {
+    if (process.env.NODE_ENV !== "production") {
+      console.log("[afrim-playground] Looks like we are in development mode!");
+    }
     window.Afrim = o.Afrim;
   })
   .catch((e) => console.error("Error importing `index.js`:", e));
