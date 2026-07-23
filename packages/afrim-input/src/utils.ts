d32c0cd3aaa228673e8c49ee08ab6834a8ac38a5
Brady Fomegne <brady.fomegne@outlook.com>
2026-07-23 01:02:17 +0100

    patch: improve user experience
    This commit improve the typing for fast typers, facilitate the usage of
    custom datasets and finally, display the downloading progress of datasets.

--- packages/afrim-input/src/utils.ts
+++ packages/afrim-input/src/utils.ts
@@ -4,15 +4,19 @@ import ky from "ky";
 
 // Convert TOML to JSON.
 export async function tomlToJson(data: string) {
-  const afrim = await require("afrim");
+  const afrim = await import("afrim");
 
   return afrim.convertTomlToJson(data);
 }
 
 // Make a http get request.
 // HTTP because we want a fast request.
-export async function httpGet(url: string) {
-  const response = await ky(url);
+export async function httpGet(url: string, downloadStatusElement: HTMLElement) {
+  const response = await ky(url, {
+    onDownloadProgress: (progress, chunk) => {
+      downloadStatusElement.textContent = `${url} | ${progress.percent * 100}% - ${progress.transferredBytes} of ${progress.totalBytes} bytes`;
+    },
+  });
 
   if (!response.ok) throw new Error(`Fetch error: ${response.statusText}`);
 
